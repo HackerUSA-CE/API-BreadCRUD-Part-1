07e010e732bf83e84316f590fe3ed055840fc3a9
Jerrica Bobadilla <jerrica@Jerricas-MBP-18.home>
2021-05-17 13:59:12 -0700

    integrate mongoose

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // DEPENDENCIES
 const express = require('express')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 
 // MIDDLEWARE
 app.use(methodOverride('_method'))
