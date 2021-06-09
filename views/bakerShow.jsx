5e712809bd57d72070f942b0e336a0b36e21ea6a
Jerrica Bobadilla <jerrica@Jerricas-iMac-13.home>
2021-06-09 10:58:15 -0700

    add bio line

--- views/bakerShow.jsx
+++ views/bakerShow.jsx
@@ -6,6 +6,7 @@ function Show ({baker}) {
       <Default>
           <h3>{baker.name}</h3>
           <p>{baker.name} has been baking with us since {baker.startDate.getFullYear()}</p>
+          <p>About {baker.name}: {baker.bio}</p>
           <h3>Breads {baker.name} has baked</h3>
           <ul>
             {
